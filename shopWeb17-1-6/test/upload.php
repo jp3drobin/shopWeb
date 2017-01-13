d25a6d6ff9d5b4592446f45596dec8c911804609
jp3drobin <89386058@qq.com>
2017-01-14 00:41:55 +0800

    4-1
    商品图片上传

---
+++ shopWeb17-1-6/test/upload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="doAction.php" method="post" enctype="multipart/form-data" >
+请选择要上传的文件: <input type="file" name="myFile"/><br/>
+<input type="submit" value="上传"/>
+</form>
+
+</body>
+</html>
